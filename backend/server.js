c28b85dcb451f46ef135670ac2c297b2d5f41809
Rohith Sri NagulaMalyala <0710rohith@gmail.com>
2024-07-21 21:36:58 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -109,6 +109,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(3001, () => {
-    console.log('Server running on port 3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
